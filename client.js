f9de2cf27c2c3e50a1178cfcc133cafe07a5349f
Josh Kralewski <joshkmanj@gmail.com>
2022-01-30 13:46:23 -0600

    Progress made on delete-button function

--- client.js
+++ client.js
@@ -9,12 +9,15 @@ $(document).ready(readyNow)
 //     annualSalary: 0
 // }
 let employeeArray =[];
+let monthlyCosts = 0;
 //initialized array for employees to be stored in
 
 
  function readyNow(){
      //Have all my inputs ready to go once page loads
-    $('.inputButton').on('click',buttonClick)
+    $('.inputButton').on('click',buttonClick);
+    $('.tableOutput').on('click', '#deleteButton', theDeleter)
+    console.log('ready now');
  }
 
  function buttonClick() {
@@ -24,20 +27,24 @@ let employeeArray =[];
         lastName:$('.lastInput').val(),
         idNumber:$('.idInput').val(),
         jobTitle:$('.titleInput').val(),
-        annualSalary: $('.salaryInput').val()
+        annualSalary: Number($('.salaryInput').val())
     }
     //This pushes employee data from the local variable to the global employee array
     employeeArray.push(employee);
 
+    //This appends employee data to the DOM
     $('.tableOutput').append(`<tr>
-    <td>first</td>
-    <td>last</td>
-    <td>id</td>
-    <td>title</td>
-    <td>salary</td>
-    <td><button id="deleteButton">Delete</button></td></tr>`)
+    <td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.idNumber}</td>
+    <td>${employee.jobTitle}</td>
+    <td class="salaryRow" >${employee.annualSalary}</td>
+    <td><button id="deleteButton" >Delete</button></td></tr>`)//appends a delete button
 
-    //appends a delete button that will delete the row it's in
+    //This refers to a funtion that'll handle the employee salary counter
+    salaryCalculator((employee.annualSalary))
+    
+
 
     //This empties input fields after having stored all data from them.
     $('.firstInput').val(''); 
@@ -50,16 +57,31 @@ let employeeArray =[];
     console.log('employee',employee);
     console.log('updated array',employeeArray);
 
-    
-    
  }//End buttonClick
 
-
- function moneyCounter() {
-    totalMoney=0;
-     for (let person of employeeArray) {
-     totalMoney += Number(person.annualSalary)
-     }
- }
-
  
+
+function theDeleter(){
+    console.log('the deleter is being run');
+
+    // console.log('This is:');
+    // $(this).parent('tableOutput');
+    // console.log('What is "this"?', ($(this).empty()));
+
+
+    
+
+    console.log('What is "this"?', ($(this).parent().child('.salaryRow')));
+    console.log('What is "this"?', ($(this).parent().parent().remove()));
+    // $(this).parent('tableOutput').empty();
+    //update salary
+    salaryCalculator(-1200);
+}
+
+function salaryCalculator(salary){
+    console.log('inide salaryCalculator');
+    
+    monthlyCosts += (salary/12)
+    $('.costOutput').empty()
+    $('.costOutput').append(monthlyCosts)
+}
