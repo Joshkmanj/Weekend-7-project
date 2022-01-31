f047994e2942b605274d432c52577b2ddbdea2d0
Josh Kralewski <joshkmanj@gmail.com>
2022-01-30 18:36:36 -0600

    Deletion function completed, additional functional improvements included

--- client.js
+++ client.js
@@ -1,27 +1,22 @@
 $(document).ready(readyNow)
 
-//this is just the outline for the employee-object
-// let employee={
-//     firstName:'', 
-//     lastName:'',
-//     idNumber:0 ,
-//     jobTitle:'' ,
-//     annualSalary: 0
-// }
+//initialized array for employees to be stored in, which isn't necessary for this particular project,
+//but can be used for more functionality later.
 let employeeArray =[];
-let monthlyCosts = 0;
-//initialized array for employees to be stored in
+//Global variable created to hold an un-abridged total of the combined annual salaries
+let totalAnnualSalary= 0;
 
 
  function readyNow(){
      //Have all my inputs ready to go once page loads
     $('.inputButton').on('click',buttonClick);
-    $('.tableOutput').on('click', '#deleteButton', theDeleter)
+    $('.tableOutput').on('click', '#deleteButton', theDeleter) //Since the delete button isn't available on page-load, parent('.tableOutput) must be targeted initially
     console.log('ready now');
  }
 
+
  function buttonClick() {
-     //This initializes an employee-object, and retrieves values from the DOM to be stored in this object
+     //This initializes an employee-object, and uses JQuery to retrieves values from the DOM to be stored in this locally stored object
      let employee={
         firstName:$('.firstInput').val(), 
         lastName:$('.lastInput').val(),
@@ -29,7 +24,8 @@ let monthlyCosts = 0;
         jobTitle:$('.titleInput').val(),
         annualSalary: Number($('.salaryInput').val())
     }
-    //This pushes employee data from the local variable to the global employee array
+
+    //This pushes employee data from the local variable to be stored on the global employee array
     employeeArray.push(employee);
 
     //This appends employee data to the DOM
@@ -41,11 +37,9 @@ let monthlyCosts = 0;
     <td class="salaryRow" >${employee.annualSalary}</td>
     <td><button id="deleteButton" >Delete</button></td></tr>`)//appends a delete button
 
-    //This refers to a funtion that'll handle the employee salary counter
+    //This calls a funtion that'll handle the employee salary counter
     salaryCalculator((employee.annualSalary))
     
-
-
     //This empties input fields after having stored all data from them.
     $('.firstInput').val(''); 
     $('.lastInput').val('');
@@ -56,32 +50,45 @@ let monthlyCosts = 0;
     //These are tests to ensure data is being input/stored properly
     console.log('employee',employee);
     console.log('updated array',employeeArray);
-
  }//End buttonClick
-
- 
 
 function theDeleter(){
     console.log('the deleter is being run');
 
-    // console.log('This is:');
-    // $(this).parent('tableOutput');
-    // console.log('What is "this"?', ($(this).empty()));
+    //This goes up to the parent of the element being clicked and then back down to the salary data cell
+    //and retrieves the text of that cell as a string, and sets it equal to a local variable 'lostSalary'
+    let lostSalary = $(this).parents('tr').children('.salaryRow').text();  //.val isn't used because that is meant for retrieving values from input fields, not html elements
 
-
+    //This multiplication will cause the string to convert into a number, which is purposely negative.
+    lostSalary= -1 * lostSalary
+    console.log('Salary being removed is:', lostSalary);
     
+    //update total salary with a negative number
+    salaryCalculator(lostSalary);
 
-    console.log('What is "this"?', ($(this).parent().child('.salaryRow')));
-    console.log('What is "this"?', ($(this).parent().parent().remove()));
-    // $(this).parent('tableOutput').empty();
-    //update salary
-    salaryCalculator(-1200);
-}
+    //This removes the parent row that contains all the employee's data.
+    $(this).parents('tr').remove();  //This jumps straight up to the ancestor that's specified in quotes, and removes it from the HTML.
+    // $(this).parent().parent().remove();// This achieves the same thing, but goes up one parent at a time.
+}//End deleter function
 
 function salaryCalculator(salary){
     console.log('inide salaryCalculator');
-    
-    monthlyCosts += (salary/12)
+
+    //This updates the global totalAnnualSalary variable
+    totalAnnualSalary += salary
+
+    //This creates a local monthlyCost variable that is converted to a string when rounded to the 2nd decimal place.
+    let monthlyCosts = (totalAnnualSalary/12).toFixed(2)
+
+    //This empties the costOutput field and appends the new value 
     $('.costOutput').empty()
-    $('.costOutput').append(monthlyCosts)
-}
+    $('.costOutput').append('$'+ monthlyCosts) //toFixed(2) makes the number round to the nearest hundredth place decimal
+
+    //This filter changes the css color based on the totalAnnualSalary, since the monthlyCosts variable is a string
+    if ( (totalAnnualSalary/12) > 20000 ){
+        console.log('Cost is too high');
+        $('.costOutput').parent().css("background-color", "red")
+    }else{
+        $('.costOutput').parent().css("background-color", "inherit")
+    }//end color filter
+}//End salaryCalculator
